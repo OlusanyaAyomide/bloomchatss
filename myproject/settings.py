fdadd61159f4849abcc7e664b072fc57f695e0b4
OlusanyaAyomide <ayomideflex72@gmail.com>
2022-08-21 21:52:10 +0100

    first

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,11 +123,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS =(os.path.join(BASE_DIR,"static"),)
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 MEDIA_ROOT=BASE_DIR/'xmedia/'
 MEDIA_URL="/user-media/"
+django_heroku.settings(locals())
